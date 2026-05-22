ab36794e9f32959f295053d276b228a1c0f462c8
Roberto Sousa <robertonssousa@gmail.com>
2026-05-22 13:57:28 +0100

    feat(core): add frame module index

---
+++ packages/core/src/frame/index.ts
@@ -0,0 +1,13 @@
+export { encode } from "./encode.ts";
+
+export {
+  type FixedLoopInfo,
+  type FixedLoopOptions,
+  fixedLoop,
+} from "./fixed-loop.ts";
+export {
+  type FrameInfo,
+  type FrameLoopHandle,
+  type LoopOptions,
+  loop,
+} from "./loop.ts";
