2ef2a8efa4e30fc9db0650a1ef130fa89081bdea
Roberto Sousa <robertonssousa@gmail.com>
2026-05-22 14:05:07 +0100

    feat(core): add gpu module index

---
+++ packages/core/src/gpu/index.ts
@@ -0,0 +1,12 @@
+export type { RequestContextOptions } from "./context.ts";
+export {
+  dispose,
+  getCurrentTextureView,
+  isDisposed,
+  requestContext,
+} from "./context.ts";
+export type { Context } from "./context-types.ts";
+export { FurnaceError, FurnaceGpuError } from "./errors.ts";
+export type { ResizeEvent } from "./resize.ts";
+
+export { onResize } from "./resize.ts";
