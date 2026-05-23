04f9401cfa5a2f44f41a15cab44d962cf194ef53
Roberto Sousa <robertonssousa@gmail.com>
2026-05-23 22:11:35 +0100

    feat(core): expose @furnace/core/mesh sub-path export

---
+++ packages/core/src/mesh/index.ts
@@ -0,0 +1,11 @@
+export { cube, plane } from "./factories.ts";
+export { createGeometry, destroyGeometry } from "./geometry.ts";
+export {
+  create,
+  destroy,
+  setPosition,
+  setRotation,
+  setScale,
+} from "./mesh.ts";
+export { cubeGeometry, planeGeometry } from "./primitives.ts";
+export type { Geometry, GeometryData, Mesh } from "./types.ts";
